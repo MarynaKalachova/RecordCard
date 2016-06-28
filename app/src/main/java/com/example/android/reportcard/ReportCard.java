5c16e121bdc38acf09258c81b647ba0485f6aef6
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-06-27 23:51:03 -0400

    Add ReportCard class

---
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -0,0 +1,40 @@
+package com.example.android.reportcard;
+
+public class ReportCard {
+
+
+    private char grade;
+    private String courseName;
+    private double courseCredit;
+
+
+    ReportCard(String courseName, double courseCredit, char grade){
+        this.courseName = courseName;
+        this.courseCredit = courseCredit;
+        this.grade = grade;
+    }
+
+    public double getCourseCredit() {
+        return courseCredit;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public char getGrade(){
+        return grade;
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public void setGrade(char grade){
+        this.grade = grade;
+    }
+
+    public void setcourseCredit (double courseCredit){
+        this.courseCredit = courseCredit;
+    }
+}
